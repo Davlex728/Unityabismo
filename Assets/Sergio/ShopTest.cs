137795000c436462d989886c02dbb3447676b9df
sergiochavarriaatienzar-prog <sergiochavarriaatienzar@gmail.com>
2025-10-28 19:09:12 +0100

    Sergio sistema de economía básica
    Al pulsar el cuadrado te va bajando el dinero, hasta que ya no tienes más.

---
+++ Assets/Sergio/ShopTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ShopTest : MonoBehaviour
+{
+    public TMP_Text textoPantalla;
+    public int dinero = 200;
+    public int precioTorre = 100;
+
+    void Start()
+    {
+        textoPantalla.text = "Dinero: " + dinero;
+    }
+
+    void OnMouseDown()
+    {
+        if (dinero >= precioTorre)
+        {
+            dinero -= precioTorre;
+            textoPantalla.text = "Compraste una torre. Dinero: " + dinero;
+            Debug.Log("Torre comprada con éxito");
+        }
+        else
+        {
+            textoPantalla.text = "No tienes suficiente dinero";
+            Debug.Log("No puedes, no tienes el dinero suficiente");
+        }
+    }
+}
